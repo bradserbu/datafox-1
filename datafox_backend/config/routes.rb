ebc5db5909607e3ad01ff54c397a78394da75fb5
lawzlo <lintan49@gmail.com>
2015-04-16 00:01:55 +0800

    can edit, show and delete company

--- datafox_backend/config/routes.rb
+++ datafox_backend/config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'main/index'
+  get 'companies/index'
 
-  get 'main/filter'
+  get 'companies/filter'
 
-  get 'main/new'
-  
-  post 'main/new'
-
-  # The priority is based upon order of creation: first created -> highest priority.
+  get 'companies/show'
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'main#index'
+  root 'companies#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -21,7 +17,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  resources :company
+  resources :companies
 
   # Example resource route with options:
   #   resources :products do
